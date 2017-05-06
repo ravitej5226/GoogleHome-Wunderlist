99343d5289be22c8c6e2d31a7c57b4d104c38693
Raghavendra_RT <raghavendra.tej@hq.cpa.com>
2017-05-05 17:06:58 -0700

    Test

--- app/models/list.js
+++ app/models/list.js
@@ -5,8 +5,8 @@ this.listItem=item
 List.prototype.GetAllLists=function(){
    var WunderlistSDK = require('wunderlist');
 var wunderlistAPI = new WunderlistSDK({
-  'accessToken': 'b5661b28d1fc6eac5f175ea93b84e7da916d8981d495828a37c87a8e092e',
-  'clientID': '71aea34e01ca73de4685'
+  'accessToken': '',
+  'clientID': ''
 });
 
 wunderlistAPI.http.lists.all()
